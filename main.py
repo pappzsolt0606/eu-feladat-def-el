603c20e708f62af571ff8b1409561ed45d98effd
pappzsolt0606 <71691840+pappzsolt0606@users.noreply.github.com>
2022-05-27 15:30:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import euclass
+from euclass import Eu
+from euclass import get_list
+from euclass import harmadik
+from euclass import negyedik
+from euclass import otodik
+from euclass import hatodik
+from euclass import hetedik
+from euclass import nyolcadik
+
+
+lista = get_list()
+print(harmadik(lista))
+print(negyedik(lista))
+print(otodik(lista))
+print(hatodik(lista))
+print(hetedik(lista))
+print(nyolcadik(lista))
